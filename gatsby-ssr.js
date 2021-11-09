f1f92649ea673c9c0e57f8aad913e5dff745c511
willm30 <64402732+willm30@users.noreply.github.com>
2021-11-09 13:08:06 +0100

    fixing ssr issue with wrapRootElemetn

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,8 @@
+const React = require("react");
+const CartProvider = require("./src/context/CartContext");
+
+const CartContextProvider = CartProvider.CartProvider;
+
 exports.wrapRootElement = ({ element }) => {
-  return <CartProvider>{element}</CartProvider>;
+  return <CartContextProvider>{element}</CartContextProvider>;
 };
