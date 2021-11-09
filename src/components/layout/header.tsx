b55415d38e31c8fdcdff0fb7ea92ef220c1299b1
willm30 <64402732+willm30@users.noreply.github.com>
2021-11-09 14:40:29 +0100

    nav height fix

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,7 +5,7 @@ import NavMenu from "../navigation/nav-menu";
 
 export default function Header() {
   return (
-    <header className="flex flex-100 bg-gray-300 text-md md:text-xl font-secular row-start-1 col-span-full max-h-16 justify-evenly">
+    <header className="flex flex-100 bg-gray-300 text-md md:text-xl font-secular row-start-1 col-span-full h-16 justify-evenly">
       <NavMenu />
     </header>
   );
